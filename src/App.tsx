33d416617982471735be4da8a86b5ae3474cde57
Alex <147102432+sobol4156@users.noreply.github.com>
2024-06-20 23:29:26 +0300

    check

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,28 @@ import { useDispatch, useSelector } from 'react-redux';
 import './App.css'
 import { useState } from 'react'
 import { AppDispatch, RootState } from './store';
+import { addTodo, deleteTodo, toggleTodo } from './features/todo/todoSlice';
 
 
 
 function App() {
   const [newTodo, setNewTodo] = useState<string>('')
 
-  const  todos = useSelector((state:RootState) => state.todos.todos)
+  const todos = useSelector((state: RootState) => state.todos.todos)
   const dispatch = useDispatch<AppDispatch>()
 
-  function addTodo() {
+  function newAddTodo() {
     if (newTodo) {
       dispatch(addTodo(newTodo))
       setNewTodo('')
     }
-  }
+  }//ну хз чё у тебя с гитом, попробуй пофиксить тут
 
-  function toggleTodo(index: number) {
+  function newToggleTodo(index: number) {
     dispatch(toggleTodo(index))
   }
 
-  function deleteTodo(index: number) {
+  function newDeleteTodo(index: number) {
     dispatch(deleteTodo(index))
   }
 
@@ -32,12 +33,12 @@ function App() {
       <h1>Todo List</h1>
       <div className="input-group">
         <input value={newTodo} onChange={(e) => { setNewTodo(e.target.value) }} type="text" id="new-todo" placeholder="Add a new task..." />
-        <button onClick={addTodo}>Add</button>
+        <button onClick={newAddTodo}>Add</button>
       </div>
       <ul id="todo-list">
         {todos.map((el, index) => (
-          <div className='list-item'>
-            <input onChange={() => toggleTodo(index)} type="checkbox" />
+          <div key={index} className='list-item'>
+            <input onChange={() => newToggleTodo(index)} type="checkbox" />
             <li
               className={el.completed ? 'completed' : ''}
               key={index}
@@ -48,7 +49,7 @@ function App() {
             </li>
             <button onClick={(e) => {
               e.stopPropagation;
-              deleteTodo(index);
+              newDeleteTodo(index);
             }}>delete</button>
           </div>
         ))}
